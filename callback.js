959067330bf7817cc39635131b847ed602ab97ae
shiva1018 <91366925+shiva1018@users.noreply.github.com>
2021-11-03 13:09:30 +0530

    callbackhell

---
+++ callback.js
@@ -0,0 +1,36 @@
+
+document.querySelector(".main").innerText=10;
+
+setInterval(() => {
+    document.querySelector(".main").innerText=9;
+    setInterval(() => {
+        document.querySelector(".main").innerText=8;
+        setInterval(() => {
+            document.querySelector(".main").innerText=7;
+            setInterval(() => {
+                document.querySelector(".main").innerText=6;
+                setInterval(() => {
+                    document.querySelector(".main").innerText=5;
+                    setInterval(() => {
+                        document.querySelector(".main").innerText=4;
+                        setInterval(() => {
+                            document.querySelector(".main").innerText=3;
+                            setInterval(() => {
+                                document.querySelector(".main").innerText=2;
+                                setInterval(() => {
+                                    document.querySelector(".main").innerText=1;
+                                    setInterval(() => {
+                                        document.querySelector(".main").innerText="Happy indenpendes Day";
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+                                 
+
+                                 
